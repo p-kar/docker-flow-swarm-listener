67b7bc743fdee87dfea00fbef04be80477d76b0e
Pratyush Kar <pratyush.kar@gmail.com>
2017-11-18 14:29:07 -0600

    main loop with docker events works great

--- main.go
+++ main.go
@@ -18,60 +18,29 @@ import (
 	"golang.org/x/net/context"
 )
 
-/*
-func decodeEvents(body io.Reader) {
-	dec := json.NewDecoder(body)
-	for {
-		var event events.Message
-		err := dec.Decode(&event)
-		if err != nil && err == io.EOF {
-			break
-		}
-		log.Println(event)
-	}
-}
-*/
-
 func main() {
 	log.Println("Starting Docker Flow Swarm Listener (now with Docker events)")
 
+	ctx := context.Background()
+	s := service.NewServiceFromEnv()
+	n:= service.NewNotificationFromEnv()
+	serve := NewServe(s, n)
+	go serve.Run()
+
+	cli := s.DockerClient
 	filters := filters.NewArgs()
 	filters.Add("type", "service")
-	/*
-	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
-	var dockerApiVersion string = "v1.22"
-	host := "unix:///var/run/docker.sock"
-	if len(os.Getenv("DF_DOCKER_HOST")) > 0 {
-		host = os.Getenv("DF_DOCKER_HOST")
-	}
-	cli, err := client.NewClient(host, dockerApiVersion, nil, defaultHeaders)
-	if err != nil {
-		panic(err)
-	}
-	*/
-	s := service.NewServiceFromEnv()
-	cli := s.DockerClient
-
-	ctx := context.Background()
-	// eventStream, errs := cli.Events(ctx, types.EventsOptions{Filters: filters,})
 	eventStream, _ := cli.Events(ctx, types.EventsOptions{Filters: filters,})
 
-	for {
-		select {
-		case msg := <-eventStream:
-			fmt.Printf("Time: %s\tType: %s\tAction: %s\tActor ID: %s\n", time.Unix(msg.Time, 0), msg.Type, msg.Action, msg.Actor.ID)
-			// log.Println(event)
-			// decodeEvents(event)
-		case <-ctx.Done():
-			log.Println("context done in main loop, exiting")
-			break
-		/*
-		case err := <-errs:
-			if err == io.EOF {
+	if len(n.CreateServiceAddr) > 0 {
+		for {
+			select {
+			case msg := <-eventStream:
+				fmt.Printf("Time: %s\tType: %s\tAction: %s\tActor ID: %s\n", time.Unix(msg.Time, 0), msg.Type, msg.Action, msg.Actor.ID)
+			case <-ctx.Done():
+				log.Println("context done in main loop, exiting")
 				break
 			}
-			log.Println("main loop error")
-		*/
 		}
 	}
 	log.Println("service terminating")
