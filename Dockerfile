05ed5098c6629cdf5d8cfdf685c8e3a81907b0df
Pratyush Kar <pratyush.kar@gmail.com>
2017-11-18 14:17:52 -0600

    basic version of event handling works

--- Dockerfile
+++ Dockerfile
@@ -22,7 +22,8 @@ EXPOSE 8080
 
 CMD ["docker-flow-swarm-listener"]
 
-HEALTHCHECK --interval=5s --start-period=3s --timeout=5s CMD wget -qO- "http://localhost:8080/v1/docker-flow-swarm-listener/ping"
+# HEALTHCHECK --interval=5s --start-period=3s --timeout=5s CMD wget -qO- "http://localhost:8080/v1/docker-flow-swarm-listener/ping"
+HEALTHCHECK NONE
 
 COPY --from=build /src/docker-flow-swarm-listener /usr/local/bin/docker-flow-swarm-listener
 RUN chmod +x /usr/local/bin/docker-flow-swarm-listener
--- main.go
+++ main.go
@@ -1,45 +1,78 @@
 package main
 
 import (
-	"./metrics"
+	// "./metrics"
 	"./service"
 	"time"
+	// for log formatting
+	// "encoding/json"
+	"fmt"
+	// "io"
+	"log"
+	//"os"
+	// Docker events API
+	//"github.com/docker/docker/client"
+	"github.com/docker/docker/api/types"
+	// "github.com/docker/docker/api/types/events"
+	"github.com/docker/docker/api/types/filters"
+	"golang.org/x/net/context"
 )
 
-func main() {
-	logPrintf("Starting Docker Flow: Swarm Listener")
-	s := service.NewServiceFromEnv()
-	n := service.NewNotificationFromEnv()
-	serve := NewServe(s, n)
-	go serve.Run()
+/*
+func decodeEvents(body io.Reader) {
+	dec := json.NewDecoder(body)
+	for {
+		var event events.Message
+		err := dec.Decode(&event)
+		if err != nil && err == io.EOF {
+			break
+		}
+		log.Println(event)
+	}
+}
+*/
 
-	args := getArgs()
-	if len(n.CreateServiceAddr) > 0 {
-		logPrintf("Starting iterations")
-		for {
-			allServices, err := s.GetServices()
-			if err != nil {
-				metrics.RecordError("GetServices")
+func main() {
+	log.Println("Starting Docker Flow Swarm Listener (now with Docker events)")
+
+	filters := filters.NewArgs()
+	filters.Add("type", "service")
+	/*
+	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
+	var dockerApiVersion string = "v1.22"
+	host := "unix:///var/run/docker.sock"
+	if len(os.Getenv("DF_DOCKER_HOST")) > 0 {
+		host = os.Getenv("DF_DOCKER_HOST")
+	}
+	cli, err := client.NewClient(host, dockerApiVersion, nil, defaultHeaders)
+	if err != nil {
+		panic(err)
+	}
+	*/
+	s := service.NewServiceFromEnv()
+	cli := s.DockerClient
+
+	ctx := context.Background()
+	// eventStream, errs := cli.Events(ctx, types.EventsOptions{Filters: filters,})
+	eventStream, _ := cli.Events(ctx, types.EventsOptions{Filters: filters,})
+
+	for {
+		select {
+		case msg := <-eventStream:
+			fmt.Printf("Time: %s\tType: %s\tAction: %s\tActor ID: %s\n", time.Unix(msg.Time, 0), msg.Type, msg.Action, msg.Actor.ID)
+			// log.Println(event)
+			// decodeEvents(event)
+		case <-ctx.Done():
+			log.Println("context done in main loop, exiting")
+			break
+		/*
+		case err := <-errs:
+			if err == io.EOF {
+				break
 			}
-			newServices, err := s.GetNewServices(allServices)
-			if err != nil {
-				metrics.RecordError("GetNewServices")
-			}
-			err = n.ServicesCreate(
-				newServices,
-				args.Retry,
-				args.RetryInterval,
-			)
-			if err != nil {
-				metrics.RecordError("ServicesCreate")
-			}
-			removedServices := s.GetRemovedServices(allServices)
-			err = n.ServicesRemove(removedServices, args.Retry, args.RetryInterval)
-			metrics.RecordService(len(service.CachedServices))
-			if err != nil {
-				metrics.RecordError("ServicesRemove")
-			}
-			time.Sleep(time.Second * time.Duration(args.Interval))
+			log.Println("main loop error")
+		*/
 		}
 	}
+	log.Println("service terminating")
 }
